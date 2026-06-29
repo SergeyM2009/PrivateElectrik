e4430af86d5e6c1d0b681afdac664d91ce0148ba
Sergey <weramor73@gmail.com>
2026-06-29 17:58:03 +0300

    верстка блока presentation

--- src/components/presentation/Presentation.jsx
+++ src/components/presentation/Presentation.jsx
@@ -58,15 +58,37 @@ export default function Presentation() {
       </span>Страховка до 100 т.р.</div>
     </div>
   </div>
- <div className={styles.main__cont}>
-     <Image
-        src="/backgrounds/happy.webp"
-        alt=""
-        fill
-        priority
-        className={styles.bg}
-      />
- </div>
+<div className={styles.main__cont}>
+  <Image
+    src="/backgrounds/happy.webp"
+    alt=""
+    fill
+    priority
+    className={styles.bg}
+  />
+
+  <div className={`container ${styles.content}`}>
+    
+    <div className={styles.left}>
+      <div className={styles.left__ready}> <span></span>Готов к выезду</div>
+      <h1 className={styles.title}> <span>Услуги электрика</span> в Москве - выезд за 30 минут</h1>
+      <p className={styles.text}>Частный электрик с опытом более 17 лет. Решу вашу проблему быстро и качественно. Бесплатный выезд и консультация.</p>
+      <div className={styles.lozung__wrapper}>
+          
+      <div><span className={styles.lozung__icon}><i className="fa-solid fa-check"></i></span>Без посредников и переплат</div>
+      <div><span className={styles.lozung__icon}><i className="fa-solid fa-check"></i></span>Гарантия до 5 лет на работы</div>
+      <div><span className={styles.lozung__icon}><i className="fa-solid fa-check"></i></span>Оплата после выполнения работ</div>
+       
+      
+
+      </div>
+    </div>
+
+    <div className={styles.right}>
+      правый блок
+    </div>
+  </div>
+</div>
   </>
   );
 }
